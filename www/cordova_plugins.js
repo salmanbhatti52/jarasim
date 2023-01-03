f59b107b7114286436cd8b9ee411b87f202abca0
moavizq5555 <57653982+Najam-khan@users.noreply.github.com>
2023-01-03 15:23:01 +0500

    commit by najam

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -318,14 +318,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "runs": true
     },
     {
-      "id": "cordova-plugin-crop.CropPlugin",
-      "file": "plugins/cordova-plugin-crop/www/crop.js",
-      "pluginId": "cordova-plugin-crop",
-      "clobbers": [
-        "plugins.crop"
-      ]
-    },
-    {
       "id": "cordova-plugin-device.device",
       "file": "plugins/cordova-plugin-device/www/device.js",
       "pluginId": "cordova-plugin-device",
@@ -431,6 +423,17 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "id": "cordova-plugin-splashscreen.SplashScreen",
       "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
       "pluginId": "cordova-plugin-splashscreen",
+<<<<<<< Updated upstream
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+=======
+>>>>>>> Stashed changes
       "clobbers": [
         "navigator.splashscreen"
       ]
@@ -441,14 +444,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "pluginId": "cordova-plugin-statusbar",
       "clobbers": [
         "window.StatusBar"
-      ]
-    },
-    {
-      "id": "cordova-plugin-telerik-imagepicker.ImagePicker",
-      "file": "plugins/cordova-plugin-telerik-imagepicker/www/imagepicker.js",
-      "pluginId": "cordova-plugin-telerik-imagepicker",
-      "clobbers": [
-        "plugins.imagePicker"
       ]
     },
     {
@@ -472,6 +467,14 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "SQLitePlugin"
       ]
+    },
+    {
+      "id": "cordova-plugin-crop.CropPlugin",
+      "file": "plugins/cordova-plugin-crop/www/crop.js",
+      "pluginId": "cordova-plugin-crop",
+      "clobbers": [
+        "plugins.crop"
+      ]
     }
   ];
   module.exports.metadata = {
@@ -484,7 +487,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-dialogs": "2.0.2",
     "cordova-plugin-nativestorage": "2.3.2",
     "cordova-plugin-apprate": "1.7.2",
-    "cordova-plugin-crop": "0.3.1",
     "cordova-plugin-device": "2.0.3",
     "cordova-plugin-email-composer": "0.10.0",
     "cordova-plugin-facebook4": "6.4.0",
@@ -498,10 +500,10 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-request-location-accuracy": "2.3.0",
     "cordova-plugin-splashscreen": "6.0.0",
     "cordova-plugin-statusbar": "3.0.0",
-    "cordova-plugin-telerik-imagepicker": "2.3.5",
     "cordova-plugin-whitelist": "1.3.5",
     "es6-promise-plugin": "4.2.2",
     "cordova-plugin-x-socialsharing": "5.6.5",
-    "cordova-sqlite-storage": "6.1.0"
+    "cordova-sqlite-storage": "6.1.0",
+    "cordova-plugin-crop": "0.3.1"
   };
 });
